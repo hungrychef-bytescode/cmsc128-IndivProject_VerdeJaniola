f72bf3e64ea048c4bbceb04abb193948b9daeeb7
Myra Verde <hungrychefbytescode@gmail.com>
2025-09-30 19:44:52 +0800

    cmsc128-Indiv-Act1-finalX

--- crud/create.py
+++ crud/create.py
@@ -1,20 +1,16 @@
 from db.connection import get_db_connection
-from crud.format import format_task 
 
 def add_task(task, timestamp, priority, status, due_date):
     try:
         with get_db_connection() as conn:
-            cursor =conn.execute("""
+            conn.execute("""
                 INSERT INTO tasks (task, timestamp, priority, status, due_date)
                     VALUES (?, ?, ?, ?, ?)""",
                     (task, timestamp, priority, status, due_date)
             )
             conn.commit()
-            task_id = cursor.lastrowid
-            inserted = conn.execute("SELECT * FROM tasks WHERE id = ?", (task_id,)).fetchone()
-            return format_task(inserted), 201
+            # print("Received:", task, timestamp, priority, status, due_date)
+            return {"success": True}
     except Exception as e:
-        print(f"Error adding task: {e}")
-        return {'error': 'Failed to add task'}, 500
-
-            
+        # print(f"Error adding task: {e}")
+        return {"error": "Failed to add task"}
--- routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .homepage import homepage_route
-from .tasks import task_routes
-
-def setup_routes(app):
-    homepage_route(app)
-    task_routes(app)
